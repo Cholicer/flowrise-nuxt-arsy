bc99f14d85d6e6d11041fea33ba1f788f3ed685e
Cholicer <cholicer@gmail.com>
2023-10-21 18:53:07 +0600

    feat: header and footer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,46 +2,46 @@ import { repositoryName } from "./slicemachine.config.json";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-   devtools: true,
+	devtools: false,
 
-   app: {
-      head: {
-         title: "Prismic + Nuxt Minimal Starter",
-         htmlAttrs: {
-            lang: "en",
-         },
-         meta: [
-            { charset: "utf-8" },
-            {
-               name: "viewport",
-               content: "width=device-width, initial-scale=1",
-            },
-            { hid: "description", name: "description", content: "" },
-            { name: "format-detection", content: "telephone=no" },
-         ],
-         link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-      },
-   },
+	app: {
+		head: {
+			title: "Prismic + Nuxt Minimal Starter",
+			htmlAttrs: {
+				lang: "en",
+			},
+			meta: [
+				{ charset: "utf-8" },
+				{
+					name: "viewport",
+					content: "width=device-width, initial-scale=1",
+				},
+				{ hid: "description", name: "description", content: "" },
+				{ name: "format-detection", content: "telephone=no" },
+			],
+			link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+		},
+	},
 
-   css: ["@fontsource-variable/nunito", "@fontsource-variable/nunito-sans"],
+	css: ["@fontsource-variable/nunito", "@fontsource-variable/nunito-sans"],
 
-   modules: ["@nuxtjs/prismic", "@nuxtjs/tailwindcss"],
+	modules: ["@nuxtjs/prismic", "@nuxtjs/tailwindcss"],
 
-   prismic: {
-      endpoint: "flowrise-nuxt-arsy",
-      preview: "/api/preview",
-      clientConfig: {
-         routes: [
-            {
-               type: "page",
-               path: "/:uid",
-            },
-            {
-               type: "page",
-               uid: "home",
-               path: "/",
-            },
-         ],
-      },
-   },
+	prismic: {
+		endpoint: "flowrise-nuxt-arsy",
+		preview: "/api/preview",
+		clientConfig: {
+			routes: [
+				{
+					type: "page",
+					path: "/:uid",
+				},
+				{
+					type: "page",
+					uid: "home",
+					path: "/",
+				},
+			],
+		},
+	},
 });
